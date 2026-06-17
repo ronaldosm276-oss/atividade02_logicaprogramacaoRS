7f4f4d1ac17e5523c5b1266080abd3f76537c2c3
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-17 20:37:01 -0300

    ainda falta a 3

---
+++ js/js3.js
@@ -0,0 +1,41 @@
+const formDados = document.querySelector('#formulario')
+const peso = document.querySelector('#peso')
+const idade = document.querySelector('#idade')
+const botão = document.querySelector('#enviar-dados')
+const resultado = document.querySelector('#result')
+
+let naoDoa = 0
+let Doa = 0
+
+
+botão.addEventListener('click', (evt) => {
+    evt.preventDefault()
+
+    
+       
+    }
+    Results.innerHTML = ``
+   
+
+)
+
+
+
+
+/*const form_num = new FormData(formDados)
+
+
+let number = parseFloat(form_num.get('numDivisores'))
+
+for (i = 1; i <= number; i++){
+    let resultado = number % i
+
+    console.log(resultado)
+    
+
+    if (resultado == 0){
+        listanum += i + ', '
+    }
+
+
+*/
