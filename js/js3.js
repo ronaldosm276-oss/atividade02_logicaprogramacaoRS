5df14a868403afcb47a7ede75947c5763e2255d1
Ronaldo S. <ronaldosm276@gmail.com>
2026-06-18 18:40:23 -0300

    feito a at3, faltou detalhes..

--- js/js3.js
+++ js/js3.js
@@ -2,40 +2,43 @@ const formDados = document.querySelector('#formulario')
 const peso = document.querySelector('#peso')
 const idade = document.querySelector('#idade')
 const botão = document.querySelector('#enviar-dados')
+const limpar = document.querySelector('#limpar')
 const resultado = document.querySelector('#result')
 
 let naoDoa = 0
 let Doa = 0
-
+let pessoas = 0
 
 botão.addEventListener('click', (evt) => {
     evt.preventDefault()
 
-    
-       
-    }
-    Results.innerHTML = ``
-   
+    let age = Number(idade.value)
+    //pega idade se for número, ou convert em número?
+    let kg = Number(peso.value)
 
-)
-
-
-
-
-/*const form_num = new FormData(formDados)
-
-
-let number = parseFloat(form_num.get('numDivisores'))
-
-for (i = 1; i <= number; i++){
-    let resultado = number % i
-
-    console.log(resultado)
-    
-
-    if (resultado == 0){
-        listanum += i + ', '
+    if (age >= 18 && age <= 60 && kg >= 50){
+        Doa++
+        
+     }
+    else{
+        naoDoa++
+        
     }
 
+    pessoas++
 
-*/
+    if (pessoas == 2) {
+        //não vou colocar 20 dados, preguiça
+        idade.setAttribute('disabled', 'disabled')
+        peso.setAttribute('disabled', 'disabled')
+        botão.setAttribute('disabled', 'disabled')
+        limpar.setAttribute('disabled', 'disabled')
+     
+        //essa parte desabilita de colocar mais dados, greys out the options when it finishes
+
+        resultado.innerHTML = `<br> total número de pessoas:${pessoas} <br> <br> <br> 
+        PODEM DOAR SANGUE: ${Doa}
+        <br>
+        NÃO PODEM DOAR SANGUE: ${naoDoa}`
+
+}})
